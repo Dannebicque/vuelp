194144e332fb6b5df196a6b76f5294e336176be8
David Annebicque <david.annebicque@gmail.com>
2021-04-21 17:36:16 +0200

    Enregistrement d'un nouveau fournisseur

--- src/views/AddFournisseur.vue
+++ src/views/AddFournisseur.vue
@@ -3,11 +3,14 @@
     <h1>Création d'un fournisseur</h1>
     <router-link :to="{name: 'Fournisseurs'}">Retour</router-link>
     <div>
+      <form>
       <input-text name="nom" label="Nom" required v-model="nom"></input-text>
       <input-text name="telephone" label="Téléphone" required v-model="telephone"></input-text>
       <input-text name="ville" label="Ville" required v-model="ville"></input-text>
-      <button @click="addFournisseur">Ajouter le fournisseur</button>
+      <button @click.prevent="addFournisseur">Ajouter le fournisseur</button>
+      </form>
     </div>
+    <div v-show="alert">Fournisseur enregistré</div>
   </div>
 </template>
 
@@ -21,17 +24,23 @@ export default {
   components: {InputText},
   data () {
     return {
+      alert:false,
       nom: '',
       telephone: '',
       ville: ''
     }
   },
   methods: {
-    addFournisseur () {
-      postFournisseur({
+    async addFournisseur () {
+      await postFournisseur({
         nom: this.nom,
         telephone: this.telephone,
         ville: this.ville
+      }).then(response => {
+        this.alert=true
+        this.nom = ''
+        this.telephone = ''
+        this.ville = ''
       })
     }
   }

2c0a121433f66f4a1d07be76947a2707574b6672
David Annebicque <david.annebicque@gmail.com>
2021-04-21 17:12:48 +0200

    Enregistrement d'un nouveau fournisseur

--- src/views/AddFournisseur.vue
+++ src/views/AddFournisseur.vue
@@ -3,9 +3,9 @@
     <h1>Création d'un fournisseur</h1>
     <router-link :to="{name: 'Fournisseurs'}">Retour</router-link>
     <div>
-      <input-text name="nom" label="Nom" required></input-text>
-      <input-text name="telephone" label="Téléphone" required></input-text>
-      <input-text name="ville" label="Ville" required></input-text>
+      <input-text name="nom" label="Nom" required v-model="nom"></input-text>
+      <input-text name="telephone" label="Téléphone" required v-model="telephone"></input-text>
+      <input-text name="ville" label="Ville" required v-model="ville"></input-text>
       <button @click="addFournisseur">Ajouter le fournisseur</button>
     </div>
   </div>
@@ -13,12 +13,26 @@
 
 <script>
 import InputText from '../components/InputText'
+import {postFournisseur} from '../services/ApiFournisseur'
+
+
 export default {
   name: 'AddFournisseur',
   components: {InputText},
+  data () {
+    return {
+      nom: '',
+      telephone: '',
+      ville: ''
+    }
+  },
   methods: {
     addFournisseur () {
-      console.log('ok')
+      postFournisseur({
+        nom: this.nom,
+        telephone: this.telephone,
+        ville: this.ville
+      })
     }
   }
 }
